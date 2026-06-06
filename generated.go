77edd364986d40a0caf05b0109c89d96c988f0eb
Stavros <steveiliop56@gmail.com>
2026-06-06 16:16:42 +0300

    fix: example fix

--- generated.go
+++ generated.go
@@ -5,5 +5,5 @@
 package main
 
 func testFn() {
-	println("world");
+	println("World");                                                                                                                                         println("i could be doing stuff here");
 }
